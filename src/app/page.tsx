c89b263a34849d9a23e8ff3fe3c63dac944d86d8
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-09-21 20:34:59 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+
+export default function HelloWorld(){
+  return(
+    
+    <h1
+    style={{
+      // transform:scale (1.9),
+      fontSize: '24px',
+      fontWeight: 'bold',
+      cursor: 'pointer',
+      textDecoration:"2px solid purple underline",
+      textAlign: "center",
+      fontFamily:"cursive",
+      
+    }}
+          >Hello World</h1>
+  )
+}
